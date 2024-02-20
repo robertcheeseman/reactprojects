1d068d97dc97005de3498299bd2caf074ed5dce2
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-19 22:23:34 -0700

    first component

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,25 +3,8 @@ import data from "./data";
 
 export default function Accordian() {
 
-    const [selected, setSelected] = useState(null);
+    return(
+        <div>Accordian</div>
+    )
 
-    function handleSingleSelection(getCurrentId){
-        console.log(getCurrentId)
     }
-
-    return <div className="wrapper">
-        <div className="accordian">
-            {
-                data && data.length > 0 ? 
-                data.map(dataItem => <div className="item">
-                    <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
-                        <h3>{dataItem.question}</h3>
-                        <span>+</span>
-                    </div>
-                </div>)
-                : <div>No Data Present</div>
-            }
-        </div>
-
-    </div>
-}
