7721b5bd5d9c1b449a9a6a651b0f0050a82e493f
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-20 21:36:46 -0700

    star rating start

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -3,21 +3,21 @@ import {FaStar} from 'react-icons/fa'
 import './styles.css'
 import { useState } from 'react'
 
-export default function StarRating({numOfStars = 10}) {
+export default function StarRating({numOfStars = 5}) {
 
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
 
     function handleClick(getCurrentIndex) {
-
+        console.log(getCurrentIndex)
     }
 
     function handleMouseEnter(getCurrentIndex) {
-
+        console.log(getCurrentIndex)
     }
 
     function handleMouseLeave(getCurrentIndex) {
-
+        console.log(getCurrentIndex)
     }
 
     return (
