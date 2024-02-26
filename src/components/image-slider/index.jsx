b3dd13fcbea599013f215863736d910f4b1584cc
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-26 15:10:44 -0700

    begin image slider

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 
 
 export default function ImageSlider({url, limit}) {
+
+    const [images, setImages] = useState([]);
+
     return (
         <div className="container">Image Slider</div>
     )
