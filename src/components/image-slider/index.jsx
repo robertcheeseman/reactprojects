ee0168e68cf34f29ca35141d15632a1800e42665
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-26 15:14:11 -0700

    image slider

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function ImageSlider({url, limit}) {
 
     const [images, setImages] = useState([]);
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    async function fetchImages(getUrl) {
+
+    };
+
+    useEffect(() => {
+        if (url !== '') fetchImages(url);
+    }, [url]);
 
     return (
         <div className="container">Image Slider</div>
