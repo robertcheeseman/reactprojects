47b84036093af75890ffcda9afba8168ffd11380
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-03-02 23:04:28 -0700

    image slider

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -5,12 +5,15 @@ export default function ImageSlider({url, limit}) {
 
     const [images, setImages] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
+    const {errorMsg, setErrorMsg} = useState(null);
 
     async function fetchImages(getUrl) {
         try {
+            const response = await fetch(getURL);
+            const data = await response.json();
 
         } catch(e) {
-
+            setErrorMsg(e.message)
         }
 
     };
