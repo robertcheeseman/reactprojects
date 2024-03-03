691e87ba4a05e7d525f7115d51bd4ca5f6ece12a
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-03-03 08:46:14 -0700

    image slider

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -29,6 +29,14 @@ export default function ImageSlider({url, limit = 5, page = 1}) {
 
     };
 
+    function handlePrevious() {
+        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+    }
+
+    function handleNext() {
+        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+    }
+
     useEffect(() => {
         if (url !== '') fetchImages(url);
     }, [url]);
@@ -45,23 +53,27 @@ export default function ImageSlider({url, limit = 5, page = 1}) {
 
     return (
         <div className="container">
-            <BsArrowLeftCircleFill className="arrow arrow-left"/>
+            <BsArrowLeftCircleFill onClick={handlePrevious} className="arrow arrow-left"/>
             {
-                images && images.length ? images.map((imageItem) => (
+                images && images.length ? images.map((imageItem, index) => (
                     <img
                     key={imageItem.id}
                     alt={imageItem.download_url}
                     src={imageItem.download_url}
-                    className="current-image"
+                    className={currentSlide === index ? "current-image" : "current-image hide-current-image"}
                     />
                 )) : null
             }
-            <BsArrowRightCircleFill className="arrow arrow-right"/>
+            <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right"/>
             <span className="circle-indicators">
                 {
                     images && images.length ? 
                     images.map((_, index) => (
-                        <button key={index} className="current-indicator"></button>
+                        <button 
+                        key={index} 
+                        className={currentSlide === index ? "current-indicator" : "current-indicator hide-current-indicator"}
+                        onClick={() => setCurrentSlide(index)}
+                        ></button>
                     ))
                     : null
                 }
