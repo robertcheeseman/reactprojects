38ee6edb631babf2480973db464973e93307ded4
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-03-06 21:53:21 -0700

    updating load more data

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react"
 
 
 export default function LoadMoreData() {
+
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    async function fetchProducts() {
+        try {
+
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
+
+            const result = await response.json();
+
+        } catch {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
+
+
     return (
         <div className="container">Load More Data</div>
     )
