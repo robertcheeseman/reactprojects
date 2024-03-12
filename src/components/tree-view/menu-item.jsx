22a29c73ea9f0618300fe22cd4692e017e96f800
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-03-11 21:48:40 -0600

    tree view

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -4,13 +4,32 @@ import {FaMinus, FaPlus} from 'react-icons/fa'
 
  export default function MenuItem({item}) {
 
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+    function handleToggleChildren(getCurrentLabel) {
+        setDisplayCurrentChildren({
+            ...displayCurrentChildren,
+            [getCurrentLabel] : !displayCurrentChildren[getCurrentLabel],
+        });
+    }
+
+    console.log(displayCurrentChildren)
+
     return (
     
     <li>
-        <p>{item.label}</p>
+        <div style={{display : 'flex', gap : '20px'}}>
+            <p>{item.label}</p>
+            {item && item.children && item.children.length ? (<span onClick={() => handleToggleChildren(item.label)}>
+            {
+                displayCurrentChildren[item.label] ? '-' : '+'
+            }
+
+            </span>) : null}
+        </div>
 
         {
-            item && item.children && item.children.length > 0 ? 
+            item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? 
             <MenuList list={item.children} />
             : null
         }
