d9960a454fe8e030d444bf47084404ce58c5dd4d
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-15 21:27:14 -0600

    github profile finder

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -3,6 +3,13 @@ import './styles.css'
 
 export default function GithubProfileFinder() {
     return (
-        <div>github profile finder</div>
+        <div className='github-profile-container'>
+            <div className='input-wrapper'>
+                <input type="text" name='search-by-username' placeholder='Search Github User Name...'/>
+                <button>
+                    Search
+                </button>
+            </div>
+        </div>
     )
 }
