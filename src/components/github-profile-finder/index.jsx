b279916217ddbd9942006a60236c8bed0b21f036
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-15 21:31:08 -0600

    github profile finder

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles.css'
 
 
 export default function GithubProfileFinder() {
     
-    const [userName, setUserName] = useState('robertcheeseman')
+    const [userName, setUserName] = useState('robertcheeseman');
+
+    
 
     function handleSubmit() {
 
     }
 
+    useEffect(()=> {
+        fetchGithubUserData()
+    }, [])
+
 
     return (
         <div className='github-profile-container'>
             <div className='input-wrapper'>
-                <input type="text" name='search-by-username' placeholder='Search Github User Name...' value={userName}/>
+                <input 
+                type="text" 
+                name='search-by-username' 
+                placeholder='Search Github User Name...' 
+                value={userName}
+                onChange={(event) => setUserName(event.target.value)}/>
                 <button onClick={handleSubmit}>
                     Search
                 </button>
