6783cd95701789763c80ac1268508528e6abc3a6
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-15 21:29:16 -0600

    github profile finder

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import './styles.css'
 
 
 export default function GithubProfileFinder() {
+    
+    const [userName, setUserName] = useState('robertcheeseman')
+
+    function handleSubmit() {
+
+    }
+
+
     return (
         <div className='github-profile-container'>
             <div className='input-wrapper'>
-                <input type="text" name='search-by-username' placeholder='Search Github User Name...'/>
-                <button>
+                <input type="text" name='search-by-username' placeholder='Search Github User Name...' value={userName}/>
+                <button onClick={handleSubmit}>
                     Search
                 </button>
             </div>
