d821a17aa9f7f3c593849775e39f785eb1c6dab2
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-17 15:55:12 -0600

    github profile finder

--- src/components/github-profile-finder/user.jsx
+++ src/components/github-profile-finder/user.jsx
@@ -1,3 +1,15 @@
 export default function User({user}) {
-    return
+    const {avatar_url, followers, following, public_repos, url, name, login} = user;
+
+
+    return (
+        <div className="user">
+            <div>
+                <img src={avatar_url} className="avatar" alt="User" />
+            </div>
+            <div>
+                <a href={`https://github.com/${login}`}>{name || login}</a>
+            </div>
+        </div>
+    )
 }
