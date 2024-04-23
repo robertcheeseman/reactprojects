6239a6d67888a70f44ce4c8c0b5d4a59b6c4cb85
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-22 22:07:12 -0600

    start search autocomplete

--- src/components/search-autocomplete-with-api/index.jsx
+++ src/components/search-autocomplete-with-api/index.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react"
 
 
 export default function SearchAutocomplete() {
 
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+
+    async function fetchListOfUsers() {
+        try {
+            const response = await fetch("https://dummyjson.com/users");
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch(error) {
+            console.log(error);
+            setError(error);
+        }
+    }
+
+    useEffect(()=> {
+        fetchListOfUsers()
+    }, []);
+
     return (
-        <div>Search Auto Complete</div>
+        <div className="search-autocomplete-container">
+            <input 
+            type="text" 
+            name="search-users"
+            placeholder="Search Users Here"
+            />
+        </div>
     )
 }
