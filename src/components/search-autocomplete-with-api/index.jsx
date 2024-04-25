768b170c2ec88c438cee96213fefc5009f29fb73
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-04-24 21:35:46 -0600

    updating

--- src/components/search-autocomplete-with-api/index.jsx
+++ src/components/search-autocomplete-with-api/index.jsx
@@ -1,36 +1,38 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 
 export default function SearchAutocomplete() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
 
-    const [loading, setLoading] = useState(false);
-    const [users, setUsers] = useState([]);
-    const [error, setError] = useState(null);
+  async function fetchListOfUsers() {
+    try {
+      setLoading(true);
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
 
-    async function fetchListOfUsers() {
-        try {
-            const response = await fetch("https://dummyjson.com/users");
-            const data = await response.json();
-
-            console.log(data);
-
-        } catch(error) {
-            console.log(error);
-            setError(error);
-        }
+      console.log(data);
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      setError(error);
     }
+  }
 
-    useEffect(()=> {
-        fetchListOfUsers()
-    }, []);
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
 
-    return (
-        <div className="search-autocomplete-container">
-            <input 
-            type="text" 
-            name="search-users"
-            placeholder="Search Users Here"
-            />
-        </div>
-    )
+  console.log(users)
+
+  return (
+    <div className="search-autocomplete-container">
+      <input type="text" name="search-users" placeholder="Search Users Here" />
+    </div>
+  );
 }
